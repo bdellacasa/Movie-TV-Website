dd86cc33075312d8bdc947691b5be672836520ea
bdellacasa <belen.dellacasa@yahoo.com.ar>
2020-04-13 15:51:25 -0300

    update carousel component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import ItemsCarousel from 'react-items-carousel';
-import '../styles/CarouselStyles.css';
+import Slider from "react-slick";
 import ClientService from '../services/ClientService';
+import '../styles/CarouselStyles.css';
+import image_not_available from '../no_image.jpeg';
 
 const IMAGE_BASE = ClientService.IMAGE_BASE_URL+ClientService.POSTER_SIZE;
 
+const placeholderItem = <div style={{ height: 300, width: 200, borderRadius: '2em', background: '#EEE' }} />;
 export default class Carousel extends Component {
   constructor(props) {
       super(props)
       this.state = {
-          items: [],
-          activeItemIndex: 0
+          items: []
       }
+      this.prev = this._prev.bind(this);
+      this.next = this._next.bind(this);
   }
 
   componentDidUpdate(prevProps) {
@@ -26,17 +29,21 @@ export default class Carousel extends Component {
       }
   }
 
+  onImgError(ev){
+    ev.target.src = image_not_available;
+  }
+
   createItems = () => {
       let date;
       return this.props.data.map((item, idx) => {
         date = new Date(item.release_date || item.first_air_date);
         return(
             <div key={idx} className={"carousel-item"}>
-                <Link className={"carousel-nav"} style={{textDecoration: 'none'}} to={`detail/${this.props.type}/${item.id}`}>
-                  <img alt="" src={IMAGE_BASE+item.poster_path} className={"carousel-item-image"}/>
-                  <div>
-                     <p style={{fontWeight: 'bold'}}>{item.title}</p>
-                     <p>{date.toDateString()}</p>
+               <Link className={"carousel-nav"} style={{textDecoration: 'none'}} to={`detail/${this.props.type}/${item.id}`}>
+                  <img onError={this.onImgError} src={IMAGE_BASE+item.poster_path} className={"carousel-item-image"}/>
+                  <div className={"carousel-item-text-container"} onClick={() => this.setState({ navigate: true, id: item.id })}>
+                      <p style={{fontWeight: 'bold'}}>{item.title || item.name}</p>
+                      <p>{date.toDateString()}</p>
                   </div>
                 </Link>
             </div>
@@ -44,35 +51,66 @@ export default class Carousel extends Component {
       })
   }
 
-  changeActiveItem = (activeItemIndex) => this.setState({ activeItemIndex });
+  _prev() {
+    this.slider.slickPrev()
+  }
+
+  _next() {
+    this.slider.slickNext()
+  }
+
+  renderPlaceholders() {
+    const items = [...Array(this.props.cardsPerSlide)].fill(placeholderItem);
+    return (
+      <div className={"carousel-container"}>
+        <p className={"carousel-title"}>{this.props.name}</p>
+        <div style={{display: 'flex', flexDirection: 'row', width: '70vw', justifyContent: 'space-between', marginLeft: '100px'}}>
+          {items}
+        </div>
+      </div>
+    );
+  }
+
+  renderContent(items) {
+    let settings = {
+      dots: this.props.dots,
+      infinite: false,
+      speed: 500,
+      slidesToShow: this.props.cardsPerSlide,
+      slidesToScroll: this.props.slidesToScroll,
+      draggable: false
+    };
+    return (
+      <div className={"carousel-container"}>
+        <p className={"carousel-title"}>{this.props.name}</p>
+        <div style={{display: 'flex', flexDirection: 'row', width: '80vw'}}>
+          <button className={"carousel-button"} style={{marginRight: '20px'}} onClick={this.prev}>{"<"}</button>
+          <Slider ref={Ref => this.slider = Ref} {...settings} style={{width: '80%'}}>
+            {items}
+          </Slider>
+          <button className={"carousel-button"} style={{marginLeft: '20px'}} onClick={this.next}>{">"}</button>
+        </div>
+      </div>
+    );
+  }
 
   render() {
     const {
-      activeItemIndex,
       items,
     } = this.state;
-
-    return (
-        <div className={"carousel-container"}>
-            <p className={"carousel-title"}>{this.props.name}</p>
-            <ItemsCarousel
-                placeholderItem={<div style={{ height: 300, width: 200, borderRadius: '2em', background: '#EEE' }} />}
-                enablePlaceholder={true}
-                numberOfPlaceholderItems={3}
-                numberOfCars={3}
-                slidesToScroll={2}
-                chevronWidth={100}
-                outsideChevron={true}
-                showSlither={false}
-                firstAndLastGutter={false}
-                activeItemIndex={activeItemIndex}
-                requestToChangeActive={value => this.setState({ activeItemIndex: value })}
-                rightChevron={'>'}
-                leftChevron={'<'}
-            >
-                {items}
-            </ItemsCarousel>
-      </div>
-    );  
+    return (items.length > 0 ? this.renderContent(items) : this.renderPlaceholders())
   }
-} 
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -5,24 +5,41 @@ import Hero from '../components/Hero';
 import Carousel from '../components/Carousel';
 import { CONTENT_TYPE } from '../Constants';
 
+const sizeScreenDependantProps = window.matchMedia("(max-width: 420px)").matches ? 
+    {
+        cardsPerSlide: 1,
+        slidesToScroll: 1,
+        dots: false
+    } :
+    {
+        cardsPerSlide: 5,
+        slidesToScroll: 3,
+        dots: true
+    };
 export class Index extends Component {
     constructor(props) {
         super(props)
         this.state = {
             carouselPopularMovies: [],
+            carouselMoviesTopRated: [],
             carouselMoviesUpcoming: [],
-            carouselPopularSeries: [],
+            carouselPopularTV: [],
+            carouselTVTopRated:[],
+            carouselTVAiringToday: [],
             heroImageUrl: undefined
         }
     }
 
     async fetchCarouselsContent() {
-        const [popularMovies, moviesUpcoming, popularSeries] = await Promise.all([ClientService.getMoviePopular(), ClientService.getMovieUpcoming(), ClientService.getTVPopular()]);
+        const [popularMovies, moviesTopRated, moviesUpcoming, popularTV, TVTopRated, TVAiringToday] = await Promise.all([ClientService.getMoviePopular(), ClientService.getMovieTopRated(), ClientService.getMovieUpcoming(), ClientService.getTVPopular(), ClientService.getTVTopRated(), ClientService.getTVAiringToday()]);
 
         this.setState({
             carouselPopularMovies: !!popularMovies ? popularMovies.results : [],
+            carouselMoviesTopRated: !!moviesTopRated ? moviesTopRated.results : [],
             carouselMoviesUpcoming: !!moviesUpcoming ? moviesUpcoming.results : [],
-            carouselPopularSeries: !!popularSeries ? popularSeries.results : [],
+            carouselPopularTV: !!popularTV ? popularTV.results : [],
+            carouselTVTopRated: !!TVTopRated ? TVTopRated.results : [],
+            carouselTVAiringToday: !!TVAiringToday ? TVAiringToday.results : [],
             heroImageUrl: !!popularMovies 
                 ?  `${ClientService.IMAGE_BASE_URL}${ClientService.BACKDROP_SIZE}${popularMovies.results[Math.floor(Math.random() * popularMovies.results.length)].backdrop_path}`
                 : ""
@@ -33,14 +50,54 @@ export class Index extends Component {
         this.fetchCarouselsContent();
         
     }
+    
 
-    renderContent() { 
+    renderContent() {
         return(
             <div>
                 <Hero imageUrl={this.state.heroImageUrl}/>
-                <Carousel data={this.state.carouselPopularMovies} name={"Popular Movies"} type={CONTENT_TYPE.MOVIES} />
-                <Carousel data={this.state.carouselMoviesUpcoming} name={"Movies upcoming"} type={CONTENT_TYPE.MOVIES}/>
-                <Carousel data={this.state.carouselPopularSeries} name={"Popular Series"} type={CONTENT_TYPE.TV}/>
+                <Carousel 
+                    data={this.state.carouselPopularMovies}
+                    name={"Popular Movies"}
+                    cardsPerSlide={sizeScreenDependantProps.cardsPerSlide}
+                    slidesToScroll={sizeScreenDependantProps.slidesToScroll}
+                    dots={sizeScreenDependantProps.dots} 
+                    type={CONTENT_TYPE.MOVIES} />
+                <Carousel 
+                    data={this.state.carouselMoviesTopRated}
+                    name={"Movies top rated"}
+                    cardsPerSlide={sizeScreenDependantProps.cardsPerSlide}
+                    slidesToScroll={sizeScreenDependantProps.slidesToScroll}
+                    dots={sizeScreenDependantProps.dots} 
+                    type={CONTENT_TYPE.MOVIES} />
+                <Carousel 
+                    data={this.state.carouselMoviesUpcoming}
+                    name={"Movies upcoming"}
+                    cardsPerSlide={sizeScreenDependantProps.cardsPerSlide}
+                    slidesToScroll={sizeScreenDependantProps.slidesToScroll}
+                    dots={sizeScreenDependantProps.dots}
+                    type={CONTENT_TYPE.MOVIES}/>
+                <Carousel 
+                    data={this.state.carouselPopularTV}
+                    name={"Popular TV"}
+                    cardsPerSlide={sizeScreenDependantProps.cardsPerSlide}
+                    slidesToScroll={sizeScreenDependantProps.slidesToScroll}
+                    dots={sizeScreenDependantProps.dots} 
+                    type={CONTENT_TYPE.TV}/>
+                <Carousel 
+                    data={this.state.carouselTVTopRated}
+                    name={"TV top rated"}
+                    cardsPerSlide={sizeScreenDependantProps.cardsPerSlide}
+                    slidesToScroll={sizeScreenDependantProps.slidesToScroll}
+                    dots={sizeScreenDependantProps.dots} 
+                    type={CONTENT_TYPE.TV}/>
+                <Carousel 
+                    data={this.state.carouselTVAiringToday}
+                    name={"TV airing today"}
+                    cardsPerSlide={sizeScreenDependantProps.cardsPerSlide}
+                    slidesToScroll={sizeScreenDependantProps.slidesToScroll}
+                    dots={sizeScreenDependantProps.dots} 
+                    type={CONTENT_TYPE.TV}/>
             </div>
         )
     }
